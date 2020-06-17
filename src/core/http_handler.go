75ae1e5b2b4eae98ad7e4fc364cece5665cc9baf
Dan Schmit <d.schmit@m3connect.de>
2020-06-17 22:58:57 +0200

    add user agent to response

--- src/core/http_handler.go
+++ src/core/http_handler.go
@@ -1,16 +1,13 @@
 package core
 
 import (
-	"fmt"
 	"net/http"
 )
 
 type HttpHandler struct{}
 
 func (h HttpHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
-	fmt.Println("[*] $ Incoming " + request.Method + " Request")
-	fmt.Println("[*] $ User Agent: " + request.Header.Get("User-Agent"))
-	data := []byte("<h1>go-http-nsaipy!</h1><p>It works!</p><a target=\"_blank\" href=\"https://github.com/woodfairy\">GitHub</a> - <a target=\"_blank\" href=\"https://danschmit.dev\">Web / Matrix</a>")
+	data := []byte("<h1>go-http-nsaipy!</h1><p>It works!</p><a target=\"_blank\" href=\"https://github.com/woodfairy\">GitHub</a> - <a target=\"_blank\" href=\"https://danschmit.dev\">Web / Matrix</a><br><br>Your UA: " + request.Header.Get("User-Agent"))
 	writer.Write(data)
 	return
 }
